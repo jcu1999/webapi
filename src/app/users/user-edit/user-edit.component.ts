9b70e49a4606c04989559818f4295df13b2b587e
Pat Carey <jcu1999@ymail.com>
2018-10-24 16:22:47 -0400

    third commit

--- src/app/users/user-edit/user-edit.component.ts
+++ src/app/users/user-edit/user-edit.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { UserService } from '../user.service';
+import { User } from '../user.class';
+import { Observable } from 'rxjs';
+import { JsonResp } from '../../json-resp.class';
 
 @Component({
   selector: 'app-user-edit',
@@ -7,9 +13,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserEditComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+
+  save(): void {
+    this.usersvc.change(this.user).subscribe(jsonresp => {
+      console.log(jsonresp);
+      if (jsonresp.rc === 200) {
+        this.router.navigateByUrl('/users/list');
+      }
+    });
+  }
+
+  constructor(
+    private usersvc: UserService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    let id = this.route.snapshot.params.id;
+    this.usersvc.get(id).subscribe(jsonresp => {
+      console.log(jsonresp);
+      this.user = jsonresp.data;
+    });
   }
 
 }
